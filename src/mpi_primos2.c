d223a95bd7221932b7fa73e4f1df17bb52dd5792
Gabriel P. Silva <gabriel.silva@gmail.com>
2026-02-09 16:57:58 -0300

    Update mpi_primos2.c

--- src/mpi_primos2.c
+++ src/mpi_primos2.c
@@ -17,27 +17,25 @@ int main(int argc, char *argv[]) {  /* mpi_primos2.c  */
         int meu_ranque, num_procs, salto;
 
         if (argc < 2) {
-                printf("Valor inválido! Entre com um valor do maior inteiro\n");
-                return 0;
+            printf("Valor inválido! Entre com um valor do maior inteiro\n");
+            return 0;
         } else
-                n = strtol(argv[1], (char **) NULL, 10);
+            n = strtol(argv[1], (char **) NULL, 10);
 
         MPI_Init(&argc, &argv);
         MPI_Comm_rank(MPI_COMM_WORLD, &meu_ranque);
         MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
+        MPI_Barrier(MPI_COMM_WORLD);
         t_inicial = MPI_Wtime();
         inicio = 3 + BLOCO*meu_ranque;
         salto = BLOCO*num_procs;
 
-        for (long int i = inicio; i <= n; i += salto)
-            for (long int j = i; (j < i+BLOCO) &&  (j < n); j+=2)
-                if(primo(j) == 1) cont++;
+        for (long int i = inicio; i <= n; i += salto)                // Distribui o trabalho em blocos menores
+             for (long int j = i; (j < i+BLOCO) &&  (j < n); j+=2)   //  para balancear a carga de trabalho
+                  if(primo(j) == 1) cont++;                          
 
-        if(num_procs > 1)
-                MPI_Reduce(&cont, &total, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-        else
-                total = cont;
-
+        MPI_Reduce(&cont, &total, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+      
         t_final = MPI_Wtime();
         if (meu_ranque == 0) {
                 total += 1;    /* Acrescenta o dois, que também é primo */
